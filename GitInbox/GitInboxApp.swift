b75d2c80df459e19fbe4b3911e055a816fc8c5ba
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2023-12-01 02:30:39 +0300

    e - New Xcode project Interface: SwiftUI; Language: Swift Unit Test on.

---
+++ GitInbox/GitInboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitInboxApp.swift
+//  GitInbox
+//
+//  Created by Mohamed Ibrahim on 01/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitInboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
