3a78f496147b11876542096b6390adc3a06e0169
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2023-12-01 10:26:14 +0300

    Change main app entry point

---
+++ GitInbox/Main.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@main
+struct Main {
+    
+    static func main() {
+//        if NSClassFromString("XCTestCase") != nil {
+//            EmptyApp.main()
+//        } else {
+            GitInboxApp.main()
+//        }
+    }
+}
