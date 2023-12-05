f49d9ad2fcfa0330f3dc074f46ca68beeec4858a
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2023-12-05 23:57:43 +0300

    Place with placeholder tests

---
+++ GitInboxTests/PlaceholderTests.swift
@@ -0,0 +1,16 @@
+//
+//  PlaceholderTests.swift
+//  GitInboxTests
+//
+//  Created by Mohamed Ibrahim on 05/12/2023.
+//
+
+import XCTest
+
+final class PlaceholderTests: XCTestCase {
+
+    func test() {
+        
+    }
+}
+
