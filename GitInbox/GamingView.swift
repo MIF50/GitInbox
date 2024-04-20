2cd2e6e30b095689bf7175a50eb6cef4572620ff
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2024-04-20 13:47:21 +0300

    Rename file

--- GitInbox/GamingView.swift
+++ GitInbox/GamingView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  GamingView.swift
 //  GitInbox
 //
 //  Created by Mohamed Ibrahim on 01/12/2023.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct GamingView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    GamingView()
 }
--- GitInbox/GitInboxApp.swift
+++ GitInbox/GitInboxApp.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct GitInboxApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            GamingView()
         }
     }
 }
