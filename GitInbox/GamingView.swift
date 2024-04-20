bb48582acd9de2ed9ce61a68ef2b9e8131370a05
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2024-04-20 13:50:56 +0300

    Create circle

--- GitInbox/GamingView.swift
+++ GitInbox/GamingView.swift
@@ -9,13 +9,7 @@ import SwiftUI
 
 struct GamingView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Circle().fill(Color.red)
     }
 }
 
